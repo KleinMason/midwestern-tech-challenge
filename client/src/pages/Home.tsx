68bff54ad32d230917e9b108a3f79b984eb20947
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2023-12-14 19:07:57 -0600

    Adding Javascript puzzle solution

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import imgRabbit from "../assets/Rabbit.png";
 import imgShield from "../assets/Shield.png";
 import imgTalkie from "../assets/Talkie.png";
@@ -6,6 +7,8 @@ import Header from "../components/Header";
 import "./css/Home.css";
 
 const Home = () => {
+  const [challengeArray, setChallengeArray] = useState(new Array<string>());
+
   const cards = [
     {
       id: 0,
@@ -34,7 +37,25 @@ const Home = () => {
   ];
 
   const handleChallengeLinkClick = () => {
-    console.log("challenge link clicked");
+    if (!!challengeArray.length)
+      return alert("Silly, you already done did made the array.");
+
+    let array1: string[] = [
+      "Matt Johnson",
+      "Matt Johnson",
+      "Bart Paden",
+      "Ryan Doss",
+      "Jared Malcolm"
+    ];
+    let array2: string[] = [
+      "Matt Johnson",
+      "Bart Paden",
+      "Bart Paden",
+      "Jordan Heigle",
+      "Jordan Heigle",
+      "Tyler Viles"
+    ];
+    setChallengeArray(Array.from(new Set([...array1, ...array2])));
   };
 
   return (
@@ -67,20 +88,16 @@ const Home = () => {
           is clicked. If the operation has been completed already, notify the
           user that this has already been done.
         </p>
+        {!!challengeArray.length && (
+          <ul>
+            {challengeArray.map((name) => (
+              <li key={name}>{name}</li>
+            ))}
+          </ul>
+        )}
       </section>
     </div>
   );
 };
 
 export default Home;
-
-/* <Card
-image={{ src: imgTalkie, alt: "talkie" }}
-heading="Heading Two"
-description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-button={{
-  label: "Learn More",
-  onClick: () => {
-    console.log("click!");
-  }
-}}></Card> */
